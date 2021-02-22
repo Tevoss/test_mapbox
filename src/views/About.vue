273268a0ff25bb44d2a2924041ae265ed2356d96
ivan.m.vasilyev@gmail.com <ivan.m.vasilyev@gmail.com>
2021-02-22 14:52:42 -0500

    this is 3d buildings along with 3d terrain

--- src/views/About.vue
+++ src/views/About.vue
@@ -16,6 +16,12 @@ export default {
   data() {
     return {
       value: null,
+      places: [
+        { lat: 55.7539, long: 37.6208, desc: "it's a square and it happens to be red" },
+        { lat: 55.7520, long: 37.6175, desc: "its an onion building"},
+        { lat: 55.7549, long: 37.5734, desc: "its a house that is white"},
+      ] 
+      
       };
     },
   mounted: function() {
@@ -26,20 +32,92 @@ export default {
      mapboxgl.accessToken = process.env.VUE_APP_MY_API_KEY;
             var map = new mapboxgl.Map({
             container: 'map', // container id
-            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            style: 'mapbox://styles/mapbox/satellite-streets-v11', // style URL
             center: [37.6173, 55.7558], // starting position [lng, lat]
             zoom: 9 // starting zoom
             });
-            var popup = new mapboxgl.Popup({ offset: 25 }).setText(
-            'Construction on the Washington Monument began in 1848.'
+            map.on('load', function () {
+            map.addSource('mapbox-dem', {
+            'type': 'raster-dem',
+            'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+            'tileSize': 512,
+            'maxzoom': 14
+            });
+            // add the DEM source as a terrain layer with exaggerated height
+            map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+            
+            // add a sky layer that will show when the map is highly pitched
+            map.addLayer({
+            'id': 'sky',
+            'type': 'sky',
+            'paint': {
+            'sky-type': 'atmosphere',
+            'sky-atmosphere-sun': [0.0, 0.0],
+            'sky-atmosphere-sun-intensity': 15
+            }
+            });
+            var layers = map.getStyle().layers;
+ 
+var labelLayerId;
+for (var i = 0; i < layers.length; i++) {
+if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+labelLayerId = layers[i].id;
+break;
+}
+}
+ 
+map.addLayer(
+{
+'id': '3d-buildings',
+'source': 'composite',
+'source-layer': 'building',
+'filter': ['==', 'extrude', 'true'],
+'type': 'fill-extrusion',
+'minzoom': 15,
+'paint': {
+'fill-extrusion-color': '#aaa',
+ 
+// use an 'interpolate' expression to add a smooth transition effect to the
+// buildings as the user zooms in
+'fill-extrusion-height': [
+'interpolate',
+['linear'],
+['zoom'],
+15,
+0,
+15.05,
+['get', 'height']
+],
+'fill-extrusion-base': [
+'interpolate',
+['linear'],
+['zoom'],
+15,
+0,
+15.05,
+['get', 'min_height']
+],
+'fill-extrusion-opacity': 0.6
+}
+},
+labelLayerId
+);
+});
+
+            
+            this.places.forEach(function(place) {
+             var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+             place.desc
             );
-            var el = document.createElement('div');
-            el.id = 'marker';
+            
             var marker = new mapboxgl.Marker()
-            .setLngLat([37.6208, 55.7539])
+            .setLngLat([place.long, place.lat])
             .setPopup(popup)
             .addTo(map);
             console.log(marker);
+            });
+            
+            
             console.log(map);
             },
             },
